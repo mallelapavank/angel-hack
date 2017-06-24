2ae1396d2f742166d3fc63e018d0bc915aa174dd
Mohammed Zubair Ahmed <ahmedzubair216@gmail.com>
2017-06-24 18:24:02 +0530

    gitignore added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import CreateCampaign from './CreateCampaign';
+import Home from './Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
